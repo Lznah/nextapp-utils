39d2325e48084862945150b6be01be38bb944a96
Lznah <hanzlpe@icloud.com>
2018-10-24 16:33:59 +0200

    test5

--- server/server.js
+++ server/server.js
@@ -8,26 +8,27 @@ var request = require('request').defaults({
 });
 const $ = require('cheerio');
 var {authenticate} = require('./middleware/authenticate');
+var Property = require('./models/property');
 var app = express();
 const port = process.env.PORT || 8000;
 //
  app.get('/', (req, res) => {
   res.send("hi");
 });
-//
-// app.get('/properties/:id', authenticate, (req, res) => {
-//   var property = new Property(req.params.id);
-//   property.run()
-//   .then((json) => {
-//     res.send(`${JSON.stringify(json)}`);
-//   })
-//   .catch((err) => {
-//     if(typeof err.status !== "undefined") {
-//       return res.status(err.status).send(`${JSON.stringify(err)}`);
-//     }
-//     res.status(400).send(`${JSON.stringify(err)}`);
-//   });
-// });
+
+app.get('/properties/:id', authenticate, (req, res) => {
+  var property = new Property(req.params.id);
+  property.run()
+  .then((json) => {
+    res.send(`${JSON.stringify(json)}`);
+  })
+  .catch((err) => {
+    if(typeof err.status !== "undefined") {
+      return res.status(err.status).send(`${JSON.stringify(err)}`);
+    }
+    res.status(400).send(`${JSON.stringify(err)}`);
+  });
+});
 
 app.listen(port, () => {
   console.log("Server is running");
