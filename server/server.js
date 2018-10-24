94bd5120e3970d58c8ef9e35196170a59d25c3b1
Lznah <hanzlpe@icloud.com>
2018-10-24 16:01:37 +0200

    test5

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var request = require('request').defaults({
   jar: true,
   headers: {
     agentOptions: {
-      ca: fs.readFileSync(path.join( __dirname, process.env.NEXTAPP_CERTIFICATE ))
+      ca: fs.readFileSync(path.join( __dirname, 'certs', 'nextappcz.cer' ))
     }
   }
 });
